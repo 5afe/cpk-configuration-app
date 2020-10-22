1c156c0a4ca6339c29b886c5bf67bea6085b4fd4
Germán Martínez <germaga92@gmail.com>
2020-10-22 17:17:14 +0200

    Updates

---
+++ precache-manifest.fa81d315a3f20a3994bf694675f3b1eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c04b7e955d149375ae53a593e468b924",
+    "url": "/cpk-configuration-app/index.html"
+  },
+  {
+    "revision": "c205229623b0876b9800",
+    "url": "/cpk-configuration-app/static/css/main.615d7b21.chunk.css"
+  },
+  {
+    "revision": "e58de5eb54b04b001a4d",
+    "url": "/cpk-configuration-app/static/js/2.77117f08.chunk.js"
+  },
+  {
+    "revision": "b0368eabd6b417500a1c24f60a61d812",
+    "url": "/cpk-configuration-app/static/js/2.77117f08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c205229623b0876b9800",
+    "url": "/cpk-configuration-app/static/js/main.61695847.chunk.js"
+  },
+  {
+    "revision": "431f1758e8895dd35417",
+    "url": "/cpk-configuration-app/static/js/runtime-main.09b4a1b8.js"
+  },
+  {
+    "revision": "47c7a29f39e976cbad1da973c9f6fc92",
+    "url": "/cpk-configuration-app/static/media/averta-bold.47c7a29f.woff2"
+  },
+  {
+    "revision": "30559e37a0ef8a886f814f588bfea868",
+    "url": "/cpk-configuration-app/static/media/averta-normal.30559e37.woff2"
+  }
+]);
